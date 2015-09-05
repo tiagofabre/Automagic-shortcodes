7187b2a58750e4eec7d471d37989770f0554aa06
tiagofabre <tiagofabre@gmail.com>
2015-09-05 19:13:36 -0300

    mostrando um popup qualquer no botão do editor

--- core/api.php
+++ core/api.php
@@ -32,13 +32,88 @@ class As_Generator {
      * Generator button
      */
     public static function button() {
-        echo '<a href="#" id="insert-my-media" class="button">Add my media</a>';
+        echo 
+        '
+        <a href="#" id="insert-my-media" class="button">Add my media</a>
+        <div id="boxXX" class="mfp-wrap mfp-close-btn-in mfp-align-top mfp-auto-cursor mfp-ready" tabindex="-1" style=" display:none; overflow-y: auto; overflow-x: hidden;"><div class="mfp-container mfp-s-ready mfp-inline-holder"><div class="mfp-content"><div id="su-generator">
+                <div id="su-generator-header">
+                    <div id="su-generator-tools"><a href="http://local.wordpress.dev/wp-admin/admin.php?page=shortcodes-ultimate#tab-1" target="_blank" title="Settings">Plugin settings</a> <span></span> <a href="http://gndev.info/shortcodes-ultimate/" target="_blank" title="Plugin homepage">Plugin homepage</a> <span></span> <a href="http://wordpress.org/support/plugin/shortcodes-ultimate/" target="_blank" title="Support forums">Support forums</a> <span></span> <a href="http://local.wordpress.dev/wp-admin/admin.php?page=shortcodes-ultimate-addons" target="_blank" title="Add-ons" class="su-add-ons">Add-ons</a></div>
+                    <input type="text" name="su_generator_search" id="su-generator-search" value="" placeholder="Search for shortcodes">
+                    <p id="su-generator-search-pro-tip"><strong>Pro Tip:</strong> Hit enter to select highlighted shortcode, while searching</p>
+                    <div id="su-generator-filter">
+                        <strong>Filter by type</strong>
+                        <a href="#" data-filter="all">All</a><a href="#" data-filter="content">Content</a><a href="#" data-filter="box">Box</a><a href="#" data-filter="media">Media</a><a href="#" data-filter="gallery">Gallery</a><a href="#" data-filter="data">Data</a><a href="#" data-filter="other">Other</a>                   </div>
+                    <div id="su-generator-choices" class="su-generator-clearfix">
+                        <span data-name="Heading" data-shortcode="heading" title="Styled heading" data-desc="Styled heading" data-group="content" style="opacity: 1;"><i class="fa fa-h-square"></i>Heading</span>
+<span data-name="Tabs" data-shortcode="tabs" title="Tabs container" data-desc="Tabs container" data-group="box" style="opacity: 1;"><i class="fa fa-list-alt"></i>Tabs</span>
+<span data-name="Tab" data-shortcode="tab" title="Single tab" data-desc="Single tab" data-group="box" style="opacity: 1;"><i class="fa fa-list-alt"></i>Tab</span>
+<span data-name="Spoiler" data-shortcode="spoiler" title="Spoiler with hidden content" data-desc="Spoiler with hidden content" data-group="box" style="opacity: 1;"><i class="fa fa-list-ul"></i>Spoiler</span>
+<span data-name="Accordion" data-shortcode="accordion" title="Accordion with spoilers" data-desc="Accordion with spoilers" data-group="box" style="opacity: 1;"><i class="fa fa-list"></i>Accordion</span>
+<span data-name="Divider" data-shortcode="divider" title="Content divider with optional TOP link" data-desc="Content divider with optional TOP link" data-group="content" style="opacity: 1;"><i class="fa fa-ellipsis-h"></i>Divider</span>
+<span data-name="Spacer" data-shortcode="spacer" title="Empty space with adjustable height" data-desc="Empty space with adjustable height" data-group="content other" style="opacity: 1;"><i class="fa fa-arrows-v"></i>Spacer</span>
+<span data-name="Highlight" data-shortcode="highlight" title="Highlighted text" data-desc="Highlighted text" data-group="content" style="opacity: 1;"><i class="fa fa-pencil"></i>Highlight</span>
+<span data-name="Label" data-shortcode="label" title="Styled label" data-desc="Styled label" data-group="content" style="opacity: 1;"><i class="fa fa-tag"></i>Label</span>
+<span data-name="Quote" data-shortcode="quote" title="Blockquote alternative" data-desc="Blockquote alternative" data-group="box" style="opacity: 1;"><i class="fa fa-quote-right"></i>Quote</span>
+<span data-name="Pullquote" data-shortcode="pullquote" title="Pullquote" data-desc="Pullquote" data-group="box" style="opacity: 1;"><i class="fa fa-quote-left"></i>Pullquote</span>
+<span data-name="Dropcap" data-shortcode="dropcap" title="Dropcap" data-desc="Dropcap" data-group="content" style="opacity: 1;"><i class="fa fa-bold"></i>Dropcap</span>
+<span data-name="Frame" data-shortcode="frame" title="Styled image frame" data-desc="Styled image frame" data-group="content" style="opacity: 1;"><i class="fa fa-picture-o"></i>Frame</span>
+<span data-name="Row" data-shortcode="row" title="Row for flexible columns" data-desc="Row for flexible columns" data-group="box" style="opacity: 1;"><i class="fa fa-columns"></i>Row</span>
+<span data-name="Column" data-shortcode="column" title="Flexible and responsive columns" data-desc="Flexible and responsive columns" data-group="box" style="opacity: 1;"><i class="fa fa-columns"></i>Column</span>
+<span data-name="List" data-shortcode="list" title="Styled unordered list" data-desc="Styled unordered list" data-group="content" style="opacity: 1;"><i class="fa fa-list-ol"></i>List</span>
+<span data-name="Button" data-shortcode="button" title="Styled button" data-desc="Styled button" data-group="content" style="opacity: 1;"><i class="fa fa-heart"></i>Button</span>
+<span data-name="Service" data-shortcode="service" title="Service box with title" data-desc="Service box with title" data-group="box" style="opacity: 1;"><i class="fa fa-check-square-o"></i>Service</span>
+<span data-name="Box" data-shortcode="box" title="Colored box with caption" data-desc="Colored box with caption" data-group="box" style="opacity: 1;"><i class="fa fa-list-alt"></i>Box</span>
+<span data-name="Note" data-shortcode="note" title="Colored box" data-desc="Colored box" data-group="box" style="opacity: 1;"><i class="fa fa-list-alt"></i>Note</span>
+<span data-name="Expand" data-shortcode="expand" title="Expandable text block" data-desc="Expandable text block" data-group="box" style="opacity: 1;"><i class="fa fa-sort-amount-asc"></i>Expand</span>
+<span data-name="Lightbox" data-shortcode="lightbox" title="Lightbox window with custom content" data-desc="Lightbox window with custom content" data-group="gallery" style="opacity: 1;"><i class="fa fa-external-link"></i>Lightbox</span>
+<span data-name="Lightbox content" data-shortcode="lightbox_content" title="Inline content for lightbox" data-desc="Inline content for lightbox" data-group="gallery" style="opacity: 1;"><i class="fa fa-external-link"></i>Lightbox content</span>
+<span data-name="Tooltip" data-shortcode="tooltip" title="Tooltip window with custom content" data-desc="Tooltip window with custom content" data-group="other" style="opacity: 1;"><i class="fa fa-comment-o"></i>Tooltip</span>
+<span data-name="Private" data-shortcode="private" title="Private note for post authors" data-desc="Private note for post authors" data-group="other" style="opacity: 1;"><i class="fa fa-lock"></i>Private</span>
+<span data-name="YouTube" data-shortcode="youtube" title="YouTube video" data-desc="YouTube video" data-group="media" style="opacity: 1;"><i class="fa fa-youtube-play"></i>YouTube</span>
+<span data-name="YouTube Advanced" data-shortcode="youtube_advanced" title="YouTube video player with advanced settings" data-desc="YouTube video player with advanced settings" data-group="media" style="opacity: 1;"><i class="fa fa-youtube-play"></i>YouTube Advanced</span>
+<span data-name="Vimeo" data-shortcode="vimeo" title="Vimeo video" data-desc="Vimeo video" data-group="media" style="opacity: 1;"><i class="fa fa-youtube-play"></i>Vimeo</span>
+<span data-name="Screenr" data-shortcode="screenr" title="Screenr video" data-desc="Screenr video" data-group="media" style="opacity: 1;"><i class="fa fa-youtube-play"></i>Screenr</span>
+<span data-name="Dailymotion" data-shortcode="dailymotion" title="Dailymotion video" data-desc="Dailymotion video" data-group="media" style="opacity: 1;"><i class="fa fa-youtube-play"></i>Dailymotion</span>
+<span data-name="Audio" data-shortcode="audio" title="Custom audio player" data-desc="Custom audio player" data-group="media" style="opacity: 1;"><i class="fa fa-play-circle"></i>Audio</span>
+<span data-name="Video" data-shortcode="video" title="Custom video player" data-desc="Custom video player" data-group="media" style="opacity: 1;"><i class="fa fa-play-circle"></i>Video</span>
+<span data-name="Table" data-shortcode="table" title="Styled table from HTML or CSV file" data-desc="Styled table from HTML or CSV file" data-group="content" style="opacity: 1;"><i class="fa fa-table"></i>Table</span>
+<span data-name="Permalink" data-shortcode="permalink" title="Permalink to specified post/page" data-desc="Permalink to specified post/page" data-group="content other" style="opacity: 1;"><i class="fa fa-link"></i>Permalink</span>
+<span data-name="Members" data-shortcode="members" title="Content for logged in members only" data-desc="Content for logged in members only" data-group="other" style="opacity: 1;"><i class="fa fa-lock"></i>Members</span>
+<span data-name="Guests" data-shortcode="guests" title="Content for guests only" data-desc="Content for guests only" data-group="other" style="opacity: 1;"><i class="fa fa-user"></i>Guests</span>
+<span data-name="RSS Feed" data-shortcode="feed" title="Feed grabber" data-desc="Feed grabber" data-group="content other" style="opacity: 1;"><i class="fa fa-rss"></i>RSS Feed</span>
+<span data-name="Menu" data-shortcode="menu" title="Custom menu by name" data-desc="Custom menu by name" data-group="other" style="opacity: 1;"><i class="fa fa-bars"></i>Menu</span>
+<span data-name="Sub pages" data-shortcode="subpages" title="List of sub pages" data-desc="List of sub pages" data-group="other" style="opacity: 1;"><i class="fa fa-bars"></i>Sub pages</span>
+<span data-name="Siblings" data-shortcode="siblings" title="List of cureent page siblings" data-desc="List of cureent page siblings" data-group="other" style="opacity: 1;"><i class="fa fa-bars"></i>Siblings</span>
+<span data-name="Document" data-shortcode="document" title="Document viewer by Google" data-desc="Document viewer by Google" data-group="media" style="opacity: 1;"><i class="fa fa-file-text"></i>Document</span>
+<span data-name="Gmap" data-shortcode="gmap" title="Maps by Google" data-desc="Maps by Google" data-group="media" style="opacity: 1;"><i class="fa fa-globe"></i>Gmap</span>
+<span data-name="Slider" data-shortcode="slider" title="Customizable image slider" data-desc="Customizable image slider" data-group="gallery" style="opacity: 1;"><i class="fa fa-picture-o"></i>Slider</span>
+<span data-name="Carousel" data-shortcode="carousel" title="Customizable image carousel" data-desc="Customizable image carousel" data-group="gallery" style="opacity: 1;"><i class="fa fa-picture-o"></i>Carousel</span>
+<span data-name="Gallery" data-shortcode="custom_gallery" title="Customizable image gallery" data-desc="Customizable image gallery" data-group="gallery" style="opacity: 1;"><i class="fa fa-picture-o"></i>Gallery</span>
+<span data-name="Posts" data-shortcode="posts" title="Custom posts query with customizable template" data-desc="Custom posts query with customizable template" data-group="other" style="opacity: 1;"><i class="fa fa-th-list"></i>Posts</span>
+<span data-name="Dummy text" data-shortcode="dummy_text" title="Text placeholder" data-desc="Text placeholder" data-group="content" style="opacity: 1;"><i class="fa fa-text-height"></i>Dummy text</span>
+<span data-name="Dummy image" data-shortcode="dummy_image" title="Image placeholder with random image" data-desc="Image placeholder with random image" data-group="content" style="opacity: 1;"><i class="fa fa-picture-o"></i>Dummy image</span>
+<span data-name="Animation" data-shortcode="animate" title="Wrapper for animation. Any nested element will be animated" data-desc="Wrapper for animation. Any nested element will be animated" data-group="other" style="opacity: 1;"><i class="fa fa-bolt"></i>Animation</span>
+<span data-name="Meta" data-shortcode="meta" title="Post meta" data-desc="Post meta" data-group="data" style="opacity: 1;"><i class="fa fa-info-circle"></i>Meta</span>
+<span data-name="User" data-shortcode="user" title="User data" data-desc="User data" data-group="data" style="opacity: 1;"><i class="fa fa-info-circle"></i>User</span>
+<span data-name="Post" data-shortcode="post" title="Post data" data-desc="Post data" data-group="data" style="opacity: 1;"><i class="fa fa-info-circle"></i>Post</span>
+<span data-name="Template" data-shortcode="template" title="Theme template" data-desc="Theme template" data-group="other" style="opacity: 1;"><i class="fa fa-puzzle-piece"></i>Template</span>
+<span data-name="QR code" data-shortcode="qrcode" title="Advanced QR code generator" data-desc="Advanced QR code generator" data-group="content" style="opacity: 1;"><i class="fa fa-qrcode"></i>QR code</span>
+<span data-name="Scheduler" data-shortcode="scheduler" title="Allows to show the content only at the specified time period" data-desc="Allows to show the content only at the specified time period" data-group="other" style="opacity: 1;"><i class="fa fa-clock-o"></i>Scheduler</span>
+                    </div>
+                </div>
+                <div id="su-generator-settings" style="display: none;"></div>
+                <input type="hidden" name="su-generator-selected" id="su-generator-selected" value="http://local.wordpress.dev/wp-content/plugins/shortcodes-ultimate">
+                <input type="hidden" name="su-generator-url" id="su-generator-url" value="http://local.wordpress.dev/wp-content/plugins/shortcodes-ultimate">
+                <input type="hidden" name="su-compatibility-mode-prefix" id="su-compatibility-mode-prefix" value="su_">
+                <div id="su-generator-result" style="display:none"></div>
+            <button title="Close (Esc)" type="button" class="mfp-close">×</button></div></div><div class="mfp-preloader">Loading...</div></div></div>
+        ';
     }
 
     public static function include_media_button_js_file() {
         wp_enqueue_script('media_button', plugins_url().'/automagic-shortcodes/core/shortcodesMCE.js', array('jquery'), '1.0', true);
     }
-    
+
 }
 ?>
 
